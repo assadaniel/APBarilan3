4d4983879b4d115e4790e8165d192921813bcf7f
assadan <danielassa411@gmail.com>
2022-08-28 11:06:34 +0300

    main server threads.

--- Mains/MainServer.cpp
+++ Mains/MainServer.cpp
@@ -1,11 +1,10 @@
 #include "Sockets/Server/SocketFileServer.h"
 #include "CLI.h"
 #include "ThreadCLI.h"
-
+#include "Sockets/Server/SocketIOServer.h"
 //
 // Created by User on 24/08/2022.
 //
-
 
 
 void* runner(void* param) {
@@ -19,17 +18,18 @@ int main() {
     SocketFileServer SFS(port);
     bool listening = true;
     std::vector<ThreadCLI *> thread_vector;
-    std::vector<SocketIO*> vSIO;
+    std::vector<SocketIOServer*> vSIO;
     for (int i = 0; i < 4; i++) { //need to do server timeout.
-        SocketIO* socketIo = new SocketIO(SFS.accept());
+        SocketIOServer* socketIo = new SocketIOServer(SFS.accept());
         vSIO.push_back(socketIo);
         ThreadCLI* t_CLI = new ThreadCLI(*socketIo);
+        thread_vector.push_back(t_CLI);
         t_CLI->start_thread();
     }
     for (ThreadCLI *t: thread_vector) {
         delete t;
     }
-    for(SocketIO* s: vSIO) {
+    for(SocketIOServer* s: vSIO) {
         delete s;
     }
 }
